627bf989c912f1f75631a4a134b85a85c316b9a8
manoj-ayyappan <manojayyappan18@gmail.com>
2025-03-28 16:20:11 -0500

    DQ 27 Mar 2025

---
+++ Hard/2503-Max_no_of_points.py
@@ -0,0 +1,28 @@
+# DQ 27 Mar 2025
+
+class Solution:
+    def maxPoints(self, grid: List[List[int]], queries: List[int]) -> List[int]:
+        ROWS, COLS = len(grid), len(grid[0])
+
+        q = [(n, i) for i, n in enumerate(queries)]
+        q.sort()
+
+        min_heap = [(grid[0][0], 0, 0)]  # val, r, c
+        visit = set([(0, 0)])
+        ans = [0] * len(queries)
+        points = 0
+
+        for limit, index in q:
+            while min_heap and min_heap[0][0] < limit:
+                val, r, c = heappop(min_heap)
+                points += 1
+                nei = [[r+1, c], [r-1, c], [r, c+1], [r, c-1]]
+                for nr, nc in nei:
+                    if (
+                        0 <= nr < ROWS and 0 <= nc < COLS and
+                        (nr, nc) not in visit
+                    ):
+                        heappush(min_heap, (grid[nr][nc], nr, nc))
+                        visit.add((nr, nc))
+            ans[index] = points
+        return ans
