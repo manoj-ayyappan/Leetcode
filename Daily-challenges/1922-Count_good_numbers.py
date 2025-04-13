f4d0b3d728aecfcb5d70b86382a885e7cca944ed
manoj-ayyappan <manojayyappan18@gmail.com>
2025-04-12 21:53:48 -0500

    DQ 12 Apr 2025

---
+++ Daily-challenges/1922-Count_good_numbers.py
@@ -0,0 +1,6 @@
+class Solution:
+    def countGoodNumbers(self, n: int) -> int:
+        MOD = 10**9 + 7
+        evens = pow(5, (n+1) // 2, MOD)
+        odds = pow(4, n // 2, MOD)
+        return (evens * odds) % MOD
