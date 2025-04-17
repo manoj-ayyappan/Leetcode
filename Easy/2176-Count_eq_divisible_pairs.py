fe7053d07afd36d0dd5482dea456f35627a6bb98
manoj-ayyappan <manojayyappan18@gmail.com>
2025-04-16 19:52:15 -0500

    DQ 16 Apr 2025

---
+++ Easy/2176-Count_eq_divisible_pairs.py
@@ -0,0 +1,12 @@
+# DQ 16 Apr 2025
+# Brute force
+# Time - O(n^2)
+
+class Solution:
+    def countPairs(self, nums: List[int], k: int) -> int:
+        ans = 0
+        for i in range(len(nums)-1):
+            for j in range(i+1, len(nums)):
+                if (nums[i] == nums[j]) and ((i*j) % k == 0):
+                    ans += 1
+        return ans
